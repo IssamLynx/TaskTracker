81a6c10fd83d824b62237b3e7feede2de2353ebb
IssamLynx <hm_bakha@esi.dz>
2023-02-19 21:55:38 +0100

    add CSS to Task component

--- src/components/Task.vue
+++ src/components/Task.vue
@@ -1,7 +1,7 @@
 <template>
     <div class="task">
     <h3>{{ task.text }}</h3>
-
+        <i class="fa fa-times"></i>
     </div>
 </template>
 
@@ -14,3 +14,23 @@ export default{
 }
 
 </script>
+
+<style>
+.fas {
+  color: red;
+}
+.task {
+  background: #f4f4f4;
+  margin: 5px;
+  padding: 10px 20px;
+  cursor: pointer;
+}
+.task.reminder {
+  border-left: 5px solid green;
+}
+.task h3 {
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+}
+</style>
