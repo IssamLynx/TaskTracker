e9bb1e930440ef49f8d466ca58c048431e2bcf71
IssamLynx <hm_bakha@esi.dz>
2023-02-19 22:47:16 +0100

    Added toggle reminder on double click

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,7 @@
 <template>
 <div class="container">
 <Header title="Task Tracker"/>
-<Tasks @delete-task="deleteTask" :tasks="tasks" />
+<Tasks @toggle-reminder="toggleReminder" @delete-task="deleteTask" :tasks="tasks" />
 </div>
 </template>
 
@@ -17,8 +17,13 @@ export default {
   },
   methods:{
     deleteTask(id){
+      if(confirm("Are you sure?")) {
       this.tasks = this.tasks.filter((task)=> task.id !== id)
     }
+  },
+  toggleReminder(id){
+    this.tasks = this.tasks.map((task)=> task.id ==id ? {...task, reminder:!task.reminder}: task)
+  }
   },
     data(){
       return {
@@ -31,6 +36,7 @@ export default {
         {
           id:"1",
           text:"Task example",
+          reminder:true,
         }
       ]
     }
